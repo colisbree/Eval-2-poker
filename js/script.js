394b15d9679ca2430936362dc7900f520374f81b
Denis <d.combe@laposte.net>
2021-03-20 18:52:59 +0100

    ajout tooltips sur le menu

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
 })
+
+// ---- PopOver ----
+$(function(){
+  $(".myPopover").popover();
+});
+
+// ---- ToolTips ----
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
